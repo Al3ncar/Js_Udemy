82cf9163b8e9a87af3a930958568a59df7a83db1
Al3ncar <alencar29htcss@gmail.com>
2022-12-15 19:46:35 -0300

    Aula 01

---
+++ Aula01/Js/index.js
@@ -0,0 +1,17 @@
+//------------ Aula 01 ----------------//
+
+console.log("Vamos Lá"); //String
+console.log("Vamos Lá"); //String
+console.log(`"Vamos"'lá'`); //String
+
+console.log(123456, 15.36, 123.5666); //Number
+console.log(`Vamos Lá`, 123456); // Number e String
+
+// Aula 01 exercicio //
+
+//MEU NOME É "NOME", ESTOU APRENDENDO "LINGUAGEM" AS "10 HORAS" DA NOITE
+
+console.log(
+  "Meu nome é Igor Alencar, Estou aprendendo Javascript as", 10,
+  `da noite no curso da Udemy`
+);
