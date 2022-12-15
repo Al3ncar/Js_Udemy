ad13b8a14e10549f3a2a3f95664bcf6243304b8e
Al3ncar <alencar29htcss@gmail.com>
2022-12-15 19:51:35 -0300

    Aula 02

---
+++ Aula02/Js/index.js
@@ -0,0 +1,22 @@
+//------------ Aula 02 ----------------//
+
+//Temos 2 tipos de comentario
+
+//linha representado por ---> //
+// console.log("Olá Mundo")
+
+console.log("Olá Mundo");
+
+//Bloco representado por ---> /* */
+/* 
+    //comentario de linha entram em um comentario de Bloco
+    console.log("Linha1")
+    console.log("Linha2")
+    console.log("Linha3")
+    console.log("Linha4") 
+*/
+
+console.log("Linha1");
+console.log("Linha2");
+console.log("Linha3");
+console.log("Linha4");
