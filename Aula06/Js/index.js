0331a87de4b16c66d32ef0ec887c9a5c6b1e11b1
Al3ncar <alencar29htcss@gmail.com>
2022-12-18 20:20:28 -0300

    Aula 06

---
+++ Aula06/Js/index.js
@@ -0,0 +1,18 @@
+var Mynome = "IGOR"; // Declarração da Variavel
+var Mynome = "Alencar"; // Redeclaração da Variavel
+
+console.log(Mynome);
+
+//ECMAScript = Padronização da Linguagem JavaScript criada pelo empresa ECMAInternational
+//ECMAScript 2015 (ES6) => versão de Padronização
+
+// a variavel "let" foi introduzida na versão ES6 da documentação do ECMAScript
+
+//Atualemnte podemos declarar uma variavel sem uma variavel
+// Variaveis: let, var, const.
+//exemplo:
+
+names = "Igor";
+// NUNCA FAZER ISSO, desta forma ira criar uma variavel Global
+//que possivelmente pode alterar outras variaveis
+console.log(names);
