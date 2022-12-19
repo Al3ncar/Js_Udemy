371fd4c5b83b8ce904afbc17860f10cb6797bfb6
Al3ncar <alencar29htcss@gmail.com>
2022-12-19 19:16:26 -0300

    Aula 11

---
+++ Aula11/Js/index.js
@@ -0,0 +1,7 @@
+// As strings são indexadas e cada caracter tem um indise
+//começando de 0 
+
+const umString = `Um texto`
+console.log(umString)
+
+//Posso escolher qual caracter quero que apareça
