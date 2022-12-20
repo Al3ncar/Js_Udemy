7c689bae9e1c01f2cf5d6d373e96aa1b7f2ab8f5
Al3ncar <alencar29htcss@gmail.com>
2022-12-19 23:15:25 -0300

    Aula 11

--- Aula11/Js/index.js
+++ Aula11/Js/index.js
@@ -1,7 +1,38 @@
-// As strings são indexadas e cada caracter tem um indise
+// As strings são indexadas e cada caracter tem um indice
 //começando de 0 
 
-const umString = `Um texto`
+const umString = `Um texto` // 01234567 = "Um Texto"
 console.log(umString)
+/*
 
 //Posso escolher qual caracter quero que apareça
+ const umString = 
+`
+ U = 0
+ m = 1
+   = 2
+ t = 3
+ e = 4
+ x = 5
+ t = 6
+ o = 7
+` 
+*/
+
+console.log(umString[4]) // e
+
+//Podemos usar tambem um método chamado de charAt()
+console.log(umString.charAt(4)) // e
+
+//Esse método charAt() retorna o caractere especificado a partir de uma string.
+
+//Temos tambem maneiras de Concatenar strings usando o método chamado de concat
+
+console.log(umString.concat(`, estou concatenando mais um texto`)) // 
+
+//podemos saber em qual indice começa a palavra "texto"
+
+console.log(umString.indexOf(`texto`)) // 3
+console.log(umString.indexOf(`to`, 3)) // 6 -> 6, 7
+
+//Posso pedir pra ele começar no indece 3, colocando um numero logo após o texto
