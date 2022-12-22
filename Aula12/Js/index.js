896d953e24ad30aafed6e9d7744b4f1f57465f91
Al3ncar <alencar29htcss@gmail.com>
2022-12-22 16:17:49 -0300

    Aula 12

--- Aula12/Js/index.js
+++ Aula12/Js/index.js
@@ -1,3 +1,5 @@
+//As contas em Js seguem o padrão IEEE 754-2008 
+
 let n1 = 2;
 let n2 = 3;
 
@@ -47,7 +49,7 @@ console.log(Number.isInteger(n5)) // inteiro = true
 console.log(Number.isInteger(n6)) // flutuante = false
 
 // ---- IsNaN() ---- //
- 
+
 // NaN => Not A Number => Não é Um Numero 
 // Podemos utilizar o ISNAN para sabermos se os valores são numeros; Ele é filho do Number;
 
@@ -58,3 +60,51 @@ console.log(isNaN("teste")) // true
 console.log(isNaN(1422)) // false
 
 //NÃO É BOA PRATICA USAR STRINGS COM NUMBERS
+
+//O Js segue uma determinada inprecisão em suas contas
+
+let n7 = 0.7
+let n8 = 0.1
+// Matematicamente era para dar 0.8
+
+console.log(n7 + n8) // 0.79999999999999999
+
+n7 += n8 // n7 = n7 + n8 //0.7999999999999
+n7 += n8 //0.8999999999999
+n7 += n8 //0.9999999999999
+
+console.log(n7)
+
+n7 = n7.toFixed(2) 
+console.log(n7) // 1.00
+//n7 ainda não irá ser um numero inteiro
+
+console.log(Number.isInteger(n7)) // false
+console.log(Number.isInteger(1.00)) // true 
+// quando o valor é literal ele retorna verdadeiro 
+
+//para realmente deixarmos ele inteiro devemos usar o Number, junto com o toFixed
+
+let n9 = 0.7;
+let n10 = 0.1;
+
+n9 += n10; // 0.799999999999 
+n9 += n10; // 0.899999999999 
+n9 += n10; // 0.999999999999 
+
+n9 = Number(n9.toFixed(2)); // 1.00 
+
+console.log(n9)
+console.log(Number.isInteger(n9));
+
+// Outra maneira
+
+let n11 = 0.7;
+let n12 = 0.1;
+
+n11 = (((n11 * 100) + (n12 * 100)) / 100) // 0.8
+n11 = (((n11 * 100) + (n12 * 100)) / 100) // 0.9
+n11 = (((n11 * 100) + (n12 * 100)) / 100) // 1
+
+console.log(n11)
+console.log(Number.isInteger(n11));
