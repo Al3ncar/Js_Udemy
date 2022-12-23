40df778a19e1f7990181628336e6498db625e949
Al3ncar <alencar29htcss@gmail.com>
2022-12-22 23:55:26 -0300

    Aula 13

--- Aula13/Js/index.js
+++ Aula13/Js/index.js
@@ -29,4 +29,26 @@ let n4 = 12
 let n5 = 70
 console.log(Math.max(1, -2, 3, 6, 45, -56, 9, -5, 17, n4 + n5 ))
 
+//min ==> ele vai buscar o menor valor de um determinada array ou sequencia 
+console.log(Math.min(1, -2, 3, 6, 45, -56, 9, -5, 17, n4 + n5 ))
+
+//random ==> ele adiciona numeros aleatorios de 0.000000 até 0.99999
+console.log(Math.random())
+
+//pi ==> Adiciona o numero de pi
+console.log(Math.PI)
+
+//pow ==> são os numeros potenciais 
+let n6  = 9
+console.log(Math.pow(2, 10)) // 1024
+console.log(2 ** 10 ) // Outra maneira
+
+// Outras maneiras usando um valor guardado
+console.log(n6 ** (1/2)) // Outra menira
+console.log(n6 ** 0.5) // Outra maneira
+
+//em JavaScript podemos dividir as coisas por 0
+console.log(100 / 0) // Infinity
+console.log(100 / 0.0000000000001) // 100000000000000000000
+
 //Método é tudo aquela função que está em um objeto
