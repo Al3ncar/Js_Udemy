265b0749c30a84f3cf7f0cb57f32fb466bdc0aab
Al3ncar <alencar29htcss@gmail.com>
2022-12-24 15:48:40 -0300

    Aula 14

--- Aula14/Js/index.js
+++ Aula14/Js/index.js
@@ -70,3 +70,45 @@ console.log(alunos4); // [ 'Geovanna', 'Melisa' ]
 const remove = alunos4.pop()
 console.log(alunos4) // [ 'Geovanna' ]
 console.log(remove) // Melisa 
+
+
+
+//MANEIRA MAIS ACESSIVEL DE EXCLUIR O PRIMEIRO ELEMENTO do array, desta maneira podemos apagar o primeiro elemento que está no meu array, exemplo: 
+
+const alunos5 = [`João`, `Maria`, `Vanessa`];
+alunos5.shift() // Sera exxcluido o primeiro elemento do Array
+ 
+console.log(alunos5); // [`Maria`, `Vanessa`]
+
+
+
+//Podemos DELETAR os Arrays usando seu indice, exemplo:
+
+const alunos6 = [`Amanda` , `Jeferson` , `Beatriz`]
+delete alunos6[1] // [`Amanda`, <Vazio> , `Beatriz`]
+
+//desta forma o elemento do indice 1 irá estar vazio. 
+
+
+//Podemos usar slice para poder renderizar o tamanho do array, exemplo:
+const alunos7 = [`Murilo`, `João`, `Jonathan`]
+
+alunos7.push("Godolfredo")
+alunos7.push("Cleidison")
+alunos7.push("Gispodol")
+console.log(alunos7)
+
+console.log(alunos7.slice(0, 3))
+
+// Caso eu use fatores negativos ele irá somar a quantidade de elementos do array - o valor escolhido
+
+console.log(alunos7.slice(0, -1))
+// 6 Elementos - 1 = 5 elemnetos 
+
+
+
+// Quando usamos typeof num Array ele renderiza Object
+// Porem aqui eu posso perguntar se alunos é uma instancia de Array
+// O instanceof é um operador testa para ver se a prototypepropriedade de um construtor aparece em qualquer lugar na cadeia de protótipo de um objeto. O valor de retorno é um valor booleano. Seu comportamento pode ser personalizado com Symbol.hasInstance.
+
+console.log(alunos7 instanceof Array) // true
