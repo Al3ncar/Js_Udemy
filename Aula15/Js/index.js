312c413d906f8649eff75249c2555ef7f2a34a50
Al3ncar <alencar29htcss@gmail.com>
2022-12-26 22:17:21 -0300

    Aula 15

--- Aula15/Js/index.js
+++ Aula15/Js/index.js
@@ -1,14 +1,26 @@
 //Funções executam ações, funções recebem e atualizam valores
 
-function saudacao(nome){ // corpo da função
+function saudacao(nome /* parametros */){ // corpo da função
     console.log(`Bom dia ${nome}`)
 }
-
-saudacao("Dodio") // Podemos passar valores para serem ultilizados na string
-
+// Podemos passar valores para serem ultilizados na string que está na funçao 
+saudacao("Dodio") 
 saudacao("Otavio")
 saudacao("Mateus")
 saudacao("Murilo")
 
 const variavel = saudacao("Pedro")
-console.log(variavel)
+
+//para que a função retorne algo devemos usar o return, exemplo:
+
+function test (names){
+    console.log(`Olá, como voce está ${names}`)
+    return(
+        `Me chamo cleidion`
+    )
+}
+
+test(`Anthony`)
+
+const people = test(`Maria`)
+console.log(people)
