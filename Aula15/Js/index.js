e96491a69edbde1c6b5d06a49a0bcee4ff15c579
Al3ncar <alencar29htcss@gmail.com>
2022-12-26 23:10:24 -0300

    Aula 15

--- Aula15/Js/index.js
+++ Aula15/Js/index.js
@@ -39,3 +39,28 @@ const Numeros = comunic(25)
 console.log(Numeros) 
 
 
+
+//Agora vamos retornar variaveis dentro da função
+
+function soma(x, y){
+    const result = x + y
+    return result;
+}
+
+console.log(soma(2, 2))
+console.log(soma(23, 7))
+console.log(soma(9, 12))
+
+//Variaveis que estão dentro da função são diferentes da fora
+
+function exemplo (x, y){
+    const exemSoma = x + y // VARIAVEL LOCAL
+    // Quando você declara uma váriavel fora de qualquer função, ela é chamada de variável global, porque está disponível para qualquer outro código no documento atual. 
+    return exemSoma
+}
+
+const exemSoma = exemplo(5, 8) // VARIAVEL GLOBAL
+// Quando você declara uma variável dentro de uma função, é chamada de variável local, pois ela está disponível somente dentro dessa função.
+console.log(exemSoma)
+
+// 
