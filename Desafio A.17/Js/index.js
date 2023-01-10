92810706977f5014a028794da9dd75f4e8a1e52f
Al3ncar <alencar29htcss@gmail.com>
2023-01-09 22:45:10 -0300

    Aula 17 DESAFIO

--- Desafio A.17/Js/index.js
+++ Desafio A.17/Js/index.js
@@ -8,12 +8,16 @@
 
 function meuEscopo(){
     const form = document.querySelector("form");
-    form.onsubmit = (e) => {e.preventDefault()}; 
-    console.log("vai caralho")
+
     //sempre que tiver um recebe " = " devemos ter um ponto e virgula " ; " no final.
     //Prevent default previne o comportamento padrão da função.
 
-    form.addEventListener()
+
+    form.onsubmit = function (e) {e.preventDefault()};
+
     // O método addEventListener() do JavaScript permite configurar funções a serem chamadas quando um evento especificado ocorre, como quando um usuário clica em um botão
+
+    function EventFunc (e) {webkitURL.preventDefault()}
+    form.addEventListener(`submit`, EventFunc )
 }
-meuEscopo()
+meuEscopo() 
