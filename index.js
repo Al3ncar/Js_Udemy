7af9bf2ff956c5d83eec71dadf10f0771d557f9e
Igor Bastos Alencar <93939408+Al3ncar@users.noreply.github.com>
2023-01-19 23:05:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// SOBRE A LÓGICA
+
+// A lógica de programação é uma coisa complicada de se compreender de primeira. Muitas pessoas já tem facilidade com lógica, outras não. Assim como matemática ou português, humanas ou exatas, alguns terão muita facilidade, outros muita dificuldade.
+
+
+/* 
+// Operadores de comparação
+
+    > maior que
+    >= maior igual
+    < menor que
+    <= menor igual 
+    == igual (valor) *************
+    === restritamente igual (valor e tipo)
+    != diferente (valor) ********
+    !== diferente estrito (valor e tipo)
+    
+*/
+
+// construção do JS 
+console.log(10 > 5) 
+// usando Logica 
+console.log(10 > 5) // 10 é maior que 5, retorna verdadeiro ou falso.
