09fda2fd77593703b26b57ff48184dfe72f1617a
Al3ncar <alencar29htcss@gmail.com>
2023-01-23 20:22:16 -0300

    Aula 18

--- Aula18/index.js
+++ Aula18/index.js
@@ -3,26 +3,52 @@
 // A lógica de programação é uma coisa complicada de se compreender de primeira. Muitas pessoas já tem facilidade com lógica, outras não. Assim como matemática ou português, humanas ou exatas, alguns terão muita facilidade, outros muita dificuldade.
 
 
-/* 
+
 // Operadores de comparação
 
-    > maior que
-    >= maior igual
-    < menor que
-    <= menor igual 
-    == igual (valor) *************
-    === restritamente igual (valor e tipo)
-    != diferente (valor) ********
-    !== diferente estrito (valor e tipo)
-    
-*/
 
-// construção do JS 
-console.log(10 > 5) 
-// usando Logica 
-console.log(10 > 5) 
+//  > maior que
+
+    console.log(10 > 5) // true
+// construção do JS usando Logica 
 // 10 é maior que 5?? retorna verdadeiro(TRUE) ou falso(FALSE)
+    const Comp = 6 > 5
+    console.log(Comp) // true
 
-const Comp = 5 > 5
-console.log(Comp)
+
+//  >= maior igual
+
+    console.log(10 >= 10) // true
+//construção do JS usando lógica
+// 10 é maior OU IGUAL a 10?? retorna verdadeiro(TRUE) ou falso(FALSE)
+
+
+//  < menor que
+
+    console.log(9 < 10) // true
+//construção do JS usando lógica
+//5 é menor que 10?? retorna verdadeiro(TRUE) ou falso(FALSE)
+
+
+//  <= menor igual 
+
+    console.log(6 <= 6) //true
+//construção do JS usando lógica
+//6 é menor igual a 6??retorna verdadeiro(TRUE) ou falso(FALSE)
+
+
+//    === restritamente igual (valor e tipo)
+
+    console.log(8 === 8) //true
+// construção do JS usando lógica
+//8 é restritamente igual a 8?? retorna verdadeiro(TRUE) ou falso(FALSE)
+
+//    !== diferente estrito (valor e tipo)
+
+
+    console.log(8 !== 8) //false
+// construção do JS usando lógica
+//8 é diferente de 8?? retorna verdadeiro(TRUE) ou falso(FALSE)
+
+
 
