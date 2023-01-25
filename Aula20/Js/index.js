0b7726ee7fe7791d2a7b9dcd107749f021708396
Al3ncar <alencar29htcss@gmail.com>
2023-01-24 23:58:55 -0300

    Aula 20

---
+++ Aula20/Js/index.js
@@ -0,0 +1,9 @@
+// Vamos usar o IF e o else, ambos são baseado em uma eztrutura de condição 
+
+const houras = 10;
+
+if(hora <= 12){
+    console.log(` Bom dia`)
+}else{
+    console.log(` Vai dormir`)
+}
