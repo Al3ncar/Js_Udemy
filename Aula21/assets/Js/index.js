d94c1df02fcac75e71e55d4e220801d0cb6a27c8
Al3ncar <alencar29htcss@gmail.com>
2023-01-26 23:17:52 -0300

    Aula 21

--- Aula21/assets/Js/index.js
+++ Aula21/assets/Js/index.js
@@ -1,4 +1,16 @@
-function meuEscop(){
-    const peso = document.querySelector(`#Peso`)
-    const altura = document.querySelector(`#Altura`)
+function Escop(){
+
+     const form = document.querySelector(`form`)
+     const result = document.querySelector(`result`)
+
+     function meuEscop(e){
+        e.preventDefault()
+        const peso = form.querySelector(`peso`)
+        const altura = form.querySelector(`altura`)
+        result.push{
+            
+        }
+     }
+     form.addEventListener(`submit`, meuEscop())
 }
+Escop()
