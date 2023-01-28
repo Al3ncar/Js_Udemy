4ad702c2377d33e6ebd4039f3f6de1716576ba1a
Al3ncar <alencar29htcss@gmail.com>
2023-01-27 23:40:26 -0300

    Aula 21

--- Aula21/assets/Js/index.js
+++ Aula21/assets/Js/index.js
@@ -7,14 +7,25 @@ function myEscop (){
 
     const peso = form.querySelector('.peso');
     const altura = form.querySelector('.altura');
-
-    const imc = peso.value
-
+    const imc = peso.value / (altura.value * altura.value).toFixed(2) 
     
-    result.innerHTML += `${peso.value} ${altura.value}`
+    /* result.innerHTML += `Seu IMC é ${imc.toFixed(2)}` */
+
+    if(imc <= 18.5 ){
+      result.innerHTML += `<p>Seu IMC é ${imc.toFixed(2)} (abaixo do peso)</p>`
+    } else if(imc <= 18.6 && imc >= 24,9){
+      result.innerHTML += `<p>Seu IMC é ${imc.toFixed(2)} (Peso normal)</p>`
+    } else if(imc <= 25 && imc >= 29,9){
+      result.innerHTML += `<p>Seu IMC é ${imc.toFixed(2)} (Sobrepeso)</p>`
+    } else if(imc <= 30 && imc >= 34.4){
+      result.innerHTML += `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)</p>`
+    } else if(imc <= 35 && 39,9){
+      result.innerHTML += `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)</p>`
+    } else if(imc >= 40){
+      result.innerHTML += `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)</p>`
+    }
     
   }
   form.addEventListener('submit', Escop)
-
 }
 myEscop()
