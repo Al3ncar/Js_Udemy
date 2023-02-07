0d787da55e9642371488d00a4e72e05f9739e232
Al3ncar <alencar29htcss@gmail.com>
2023-02-07 13:58:27 -0300

    Aula 21

--- Aula21/assets/Js/index.js
+++ Aula21/assets/Js/index.js
@@ -7,24 +7,24 @@ function myEscop (){
 
     const peso = form.querySelector('.peso');
     const altura = form.querySelector('.altura');
-    const imc = peso.value / Number(altura.value) * Number(altura.value); // resultado do imc 
+    const imc = peso.value / (altura.value * altura.value); // resultado do imc 
     function imc2(){
       if(peso.value === "" || peso.value >= 596){
-        result.innerHTML += `<li class=".msgErro">peso invalido ou altura invalido </li>`
+        result.innerHTML = `<span>peso invalido ou altura invalido </span>`
       } else if(altura.value === "" || altura.value >= 2.52){
-        result.innerHTML += `<li class=".msgErro">altura invalido ou peso invalido</li> `
+        result.innerHTML = `<span>altura invalido ou peso invalido</span> `
       } else if(imc <= 18.5){
-        result.innerHTML += `<li>Seu IMC é ${imc.toFixed(2)} (abaixo do peso)</li>`
-      } else if(imc <= 18.6 && imc >= 24,9){
-        result.innerHTML += `<li>Seu IMC é ${imc.toFixed(2)} (Peso normal)</li>`
-      } else if(imc <= 25 && imc >= 29,9){
-        result.innerHTML += `<li>Seu IMC é ${imc.toFixed(2)} (Sobrepeso)</li>`
-      } else if(imc <= 30 && imc >= 34.4){
-        result.innerHTML += `<li>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)</li>`
-      } else if(imc <= 35 && 39,9){
-        result.innerHTML += `<li>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)</li>`
+        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (abaixo do peso)</p>`
+      } else if(imc <= 18.6){
+        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Peso normal)</p>`
+      } else if(imc <= 25){
+        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Sobrepeso)</p>`
+      } else if(imc <= 30){
+        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)</p>`
+      } else if(imc <= 35){
+        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)</p>`
       } else if(imc >= 40){
-        result.innerHTML += `<li>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)</li>`
+        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)</p>`
       }
     }imc2()
   }
@@ -32,3 +32,10 @@ function myEscop (){
 }
 myEscop()
 
+/* 
+
+
+
+
+
+*/
