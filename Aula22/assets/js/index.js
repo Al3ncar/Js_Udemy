f2cc2301d344ff0a7c612dced0ecc9d703e6744b
Al3ncar <alencar29htcss@gmail.com>
2023-02-08 12:57:17 -0300

    Aula 22

--- Aula22/assets/js/index.js
+++ Aula22/assets/js/index.js
@@ -1,16 +1,27 @@
 function Escop(){
-    const add = document.querySelector('#ativar').addEventListener('click', addButon)
+    const btnAdd = document.querySelector('#ativar').addEventListener('click', addButon)
+    const btnRemove = document.querySelector('#remover').addEventListener('click', remov)
 
     function addButon(){
         let e = document.querySelector('#executar')
 
         e.addEventListener('click', Exec)
-        e.addEventListener('click', )
+        e.addEventListener('click', Exec2)
     } 
 
     function Exec(){
         let temp = new Date()
-        document.querySelector('#time')
-        document.querySelector('#time2')
+        document.querySelector('#time').textContent = temp.getSeconds()
+        
     }
-}
+
+    function Exec2(){
+        let temp = new Date()
+        document.querySelector('#time2').textContent = temp.getSeconds() 
+    }
+
+    function remov(){
+        let eRemove = document.querySelector('#executar').removeEventListener('click', Exec)
+    }
+    
+} Escop()
