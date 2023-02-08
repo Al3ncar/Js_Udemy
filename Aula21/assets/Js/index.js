d360378b07b01395506834e08fb9a95fa7415bc1
Al3ncar <alencar29htcss@gmail.com>
2023-02-08 16:22:21 -0300

    Aula 21

--- Aula21/assets/Js/index.js
+++ Aula21/assets/Js/index.js
@@ -15,13 +15,13 @@ function myEscop (){
         result.innerHTML = `<span>altura invalido ou peso invalido</span> `
       } else if(imc <= 18.5){
         result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (abaixo do peso)</p>`
-      } else if(imc <= 18.6){
+      } else if(imc >= 18.6 && imc <= 24.9){
         result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Peso normal)</p>`
-      } else if(imc <= 25){
+      } else if(imc >= 25 && imc <= 29.9){
         result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Sobrepeso)</p>`
-      } else if(imc <= 30){
+      } else if(imc >= 30 && imc <= 34,9){
         result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)</p>`
-      } else if(imc <= 35){
+      } else if(imc >= 35 && imc <= 39,9){
         result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)</p>`
       } else if(imc >= 40){
         result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)</p>`
@@ -29,13 +29,4 @@ function myEscop (){
     }imc2()
   }
   form.addEventListener('submit', Escop)
-}
-myEscop()
-
-/* 
-
-
-
-
-
-*/
+} myEscop()
