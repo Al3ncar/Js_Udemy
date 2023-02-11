1efafd0453f560cf3dee44951163f61637295629
Al3ncar <alencar29htcss@gmail.com>
2023-02-11 13:45:58 -0300

    Aula 21

--- Aula21/assets/Js/index.js
+++ Aula21/assets/Js/index.js
@@ -13,24 +13,7 @@ function myEscop (){
     const imc = peso / (altura * altura); // resultado do imc 
     console.log(peso, altura)
     function imc2(){
-/*        if(!peso || peso >= 596 || peso <= 2.1){
-        result.innerHTML = `<span>peso invalido</span>`
-      } else if(!altura || altura >= 2.52 || altura <= 0.67){
-        result.innerHTML = `<span>altura invalida</span> `
-      } else if(imc <= 18.5){
-        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (abaixo do peso)</p>`
-      } else if(imc >= 18.6 && imc <= 24.9){
-        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Peso normal)</p>`
-      } else if(imc >= 25 && imc <= 29.9){
-        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Sobrepeso)</p>`
-      } else if(imc >= 30 && imc <= 34.9){
-        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)</p>`
-      } else if(imc >= 35 && imc <= 39.9){
-        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)</p>`
-      } else if(imc >= 40){
-        result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)</p>`
-      }  */
-
+      // Usando Ternario
       !peso || peso >= 596 || peso <= 2.1 ? result.innerHTML = `<span>peso invalido</span>`: 
       !altura || altura >= 2.52 || altura <= 0.67 ? result.innerHTML = `<span>altura invalida</span> `:
       imc <= 18.5 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (abaixo do peso)</p>`:
@@ -39,6 +22,7 @@ function myEscop (){
       imc >= 30 && imc <= 34.9 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)</p>`:
       imc >= 35 && imc <= 39.9 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)</p>`:
       imc >= 40 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)</p>` : null;
+
     }imc2()
   }
   form.addEventListener('submit', Escop)
