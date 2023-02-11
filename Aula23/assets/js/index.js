e693aabef64a50262c8f67b7875e9ca79c3be75d
Al3ncar <alencar29htcss@gmail.com>
2023-02-11 08:01:26 -0300

    Aula 23

--- Aula23/assets/js/index.js
+++ Aula23/assets/js/index.js
@@ -2,17 +2,24 @@
 
 //Eẽ é uma propriedade do node, cujo renderiza a interface do conteudo, ou seja, mostra texto na tela, sendo bem semelhante ao InnerHtml, um exemplo de sintaxw:
 
-let text = document.querySelector('.text')
-text.textContent = `Olá, como você está??`
+let text = document.querySelector('.text');
+text.textContent = `Olá, como você está??`;
 
 //tela irá retornar um "Olá, como você está??""
 //o textContent é mais performatico do que o innerHTML, tendo uma sintaaxe de construção bem semelhante ao do exemplo acima:
 
-let text2 = document.querySelector('.text')
-text2.innerHTML = `Olá, estou bem, e você?? como você está??`
+let text2 = document.querySelector('.text');
+text2.innerHTML = `Olá, estou bem, e você?? como você está??`;
 
 // createElement() adiciona é um metodo que cria elementos HTML, está relacionado com outro método, chamado de appendChild()
 // No exemplo a seguir pode ver como funciona a criação de um elemento e como ele é coloca no HTML.
+
+
+// ----   ----  //
+
+
+
+//createElement()
 
 //Criando elemento:
 let p = document.createElement('p');
@@ -29,10 +36,15 @@ text.appendChild(p)
 const p2 = document.createElement('div');
 
 //Agora iremos colocar uma classe na div
-p2.classList = "div_list"
+
+// 1° jeito: 
+p2.classList = "div_list";
+
+//2° jeito:
+p2.classList.add(`div_list2`)
 
 //Colocamos eles no HTML,  inserindo elementos filhos 
-const conteiner = document.querySelector('.cont')
+const conteiner = document.querySelector('.cont');
 conteiner.appendChild(p2) 
 
 
@@ -42,7 +54,7 @@ const el = document.createElement('div');
 
 el.classList = "div_el";
 
-const el3 = document.querySelector('.cont .div_list')
+const el3 = document.querySelector('.cont .div_list');
 
 console.log(el3)
 
