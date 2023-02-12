bc68a412778f0acf41bc6ff74e80694a4d7b06bc
Al3ncar <alencar29htcss@gmail.com>
2023-02-11 23:46:44 -0300

    Aula 21

--- Aula21/assets/Js/index.js
+++ Aula21/assets/Js/index.js
@@ -14,14 +14,14 @@ function myEscop (){
     console.log(peso, altura)
     function imc2(){
       // Usando Ternario
-      !peso || peso >= 596 || peso <= 2.1 ? result.innerHTML = `<span>peso invalido</span>`: 
-      !altura || altura >= 2.52 || altura <= 0.67 ? result.innerHTML = `<span>altura invalida</span> `:
-      imc <= 18.5 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (abaixo do peso)</p>`:
-      imc >= 18.6 && imc <= 24.9 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Peso normal)</p>`:
-      imc >= 25 && imc <= 29.9 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Sobrepeso)</p>`:
-      imc >= 30 && imc <= 34.9 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)</p>`:
-      imc >= 35 && imc <= 39.9 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)</p>`:
-      imc >= 40 ? result.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)</p>` : null;
+      !peso || peso >= 596 || peso <= 2.1 ? result.textContent = `<span>peso invalido</span>`: 
+      !altura || altura >= 2.52 || altura <= 0.67 ? result.textContent = `<span>altura invalida</span> `:
+      imc <= 18.5 ? result.textContent = `<p>Seu IMC é ${imc.toFixed(2)} (abaixo do peso)</p>`:
+      imc >= 18.6 && imc <= 24.9 ? result.textContent = `<p>Seu IMC é ${imc.toFixed(2)} (Peso normal)</p>`:
+      imc >= 25 && imc <= 29.9 ? result.textContent = `<p>Seu IMC é ${imc.toFixed(2)} (Sobrepeso)</p>`:
+      imc >= 30 && imc <= 34.9 ? result.textContent = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)</p>`:
+      imc >= 35 && imc <= 39.9 ? result.textContent = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)</p>`:
+      imc >= 40 ? result.textContent = `<p>Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)</p>` : null;
 
     }imc2()
   }
