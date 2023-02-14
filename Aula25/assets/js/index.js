b112cc026b37534ff82c407bcbf167c2dc66b0da
Al3ncar <alencar29htcss@gmail.com>
2023-02-13 23:58:29 -0300

    Aula 25

--- Aula25/assets/js/index.js
+++ Aula25/assets/js/index.js
@@ -11,3 +11,8 @@ const treshoras = 60 * 60 * 3 * 1000;
 const umDia = 60 * 60 * 24 * 1000;
 const data2 = new Date(0 + treshoras) // 01 / 01 / 1970 Timestamp unix = ou época unix
 console.log(data2.toString()) 
+
+
+
+const data3 = new Date(2019, 4, 20, 20, 20, 59, 999)
+// construção logica  (ANO, MES, DIA, HORAS, MINUTOS, SEGUNDO, MILISEGUNDOS)
