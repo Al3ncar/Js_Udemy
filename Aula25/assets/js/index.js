cbead0d81034c6b60a2b1793aa5e2eb65d039321
Al3ncar <alencar29htcss@gmail.com>
2023-02-13 23:39:25 -0300

    Aula 25

--- Aula25/assets/js/index.js
+++ Aula25/assets/js/index.js
@@ -4,3 +4,10 @@
 const data = new Date(0)//  01/01/1970 Timestamp unix ou época unix 
 console.log(data.toString()) 
 // devido os horarios, notamos que ira retornar 31/12/1969 devido estarmos 3 horas atrasados
+
+//Para termos o inicio do timestamp unix, devemos construir um codigo cujo está 3 horas afrente
+
+const treshoras = 60 * 60 * 3 * 1000; 
+const umDia = 60 * 60 * 24 * 1000;
+const data2 = new Date(0 + treshoras) // 01 / 01 / 1970 Timestamp unix = ou época unix
+console.log(data2.toString()) 
