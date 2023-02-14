fd98497d2882590654a81a59d0aed0be9424fbbb
Al3ncar <alencar29htcss@gmail.com>
2023-02-13 23:04:35 -0300

    Aula 25

---
+++ Aula25/assets/js/index.js
@@ -0,0 +1,6 @@
+// Objeto Date
+// Ela é uma função construtora
+
+const data = new Date(0)//  01/01/1970 Timestamp unix ou época unix 
+console.log(data.toString()) 
+// devido os horarios, notamos que ira retornar 31/12/1969 devido estarmos 3 horas atrasados
