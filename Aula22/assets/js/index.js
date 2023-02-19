440e3a6c3ef3f586d68eb5d1b3afe821c175de2c
Al3ncar <alencar29htcss@gmail.com>
2023-02-18 21:39:46 -0300

    Aula 22

--- Aula22/assets/js/index.js
+++ Aula22/assets/js/index.js
@@ -25,8 +25,12 @@ function Escop(){
         e.addEventListener('click', action1);
         e.addEventListener('click', action2);
     }
-
     function action1(){
-        let
+        let t = new Date();
+        document.querySelector('#time').textContent = t.getSeconds();
+    }
+    function action2(){  
+        let t = new Date();
+        document.querySelector('#time2').textContent = t.getSeconds();
     }
 }Escop()
