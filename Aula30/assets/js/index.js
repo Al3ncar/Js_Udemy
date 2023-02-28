4e8b3ca9a9b90ab74482afd3aaaea89ec1ff744d
Al3ncar <alencar29htcss@gmail.com>
2023-02-28 20:40:41 -0300

    Aula 30

--- Aula30/assets/js/index.js
+++ Aula30/assets/js/index.js
@@ -20,15 +20,23 @@ const pessoas2 = {
     nome: 'Igor',
     sobrenome:'Alencar',
     idade:30,
-    endereço:{
+    endereco:{
         rua: 'Av Sâo Miguel',
         numero: 123
     }
 };
 //podemos colocar valores padrão como
+
 //      valor padrão
 const { nome = '', sobrenome, idade} = pessoas2;
 console.log(nome, sobrenome, idade);
+
 //Podemos alterar o nome usando chave
+
 const { nome: teste /* deste modo troquei a palavra NOME por TESTE */, sobrenomes, idades} = pessoas2;
 console.log(teste, sobrenomes, idades ) 
+
+//Para pegar o valor do endereco decemos desenvolver os seguinte codigo:
+
+const { endereco: { rua, numero } } = pessoas2;
+console.log(rua, numero);
