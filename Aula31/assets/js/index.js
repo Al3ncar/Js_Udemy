90d0b16cea1e5099550ab893ab217d8667e8c1da
Al3ncar <alencar29htcss@gmail.com>
2023-03-01 19:02:16 -0300

    Aula 31

---
+++ Aula31/assets/js/index.js
@@ -0,0 +1,18 @@
+// Estrutura de reptição
+
+console.log('Linha 1')
+console.log('Linha 2')
+console.log('Linha 3')
+console.log('Linha 4')
+console.log('Linha 5')
+
+console.log(' ')// Espaço no consele
+
+
+// O padrão I é de INDEX
+for(
+    /*Criar um variavel*/ let i = 0; 
+    /* criar uma condição */ i <= 5 ; 
+    /* incrementar ou decrementar a variavel */ i++
+){  console.log(`Linha ${i}`); }
+
