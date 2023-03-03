f9f6eb38e4eecc5ac19561ca5cc4f071c7dd713b
Al3ncar <alencar29htcss@gmail.com>
2023-03-02 23:52:56 -0300

    Aula 32

--- Aula32/assets/js/index.js
+++ Aula32/assets/js/index.js
@@ -4,3 +4,12 @@ const elementos = [
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'},
 ]
+
+const [t1, t2, t3, t4 ] = [, [tag, texto], [tag, texto], [tag, texto]] 
+
+console.log(elementos.length)
+
+console.log(t1)
+console.log(t2)
+console.log(t3)
+console.log(t4)
