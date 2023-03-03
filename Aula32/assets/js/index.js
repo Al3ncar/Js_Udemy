fbb1336e7b85a514caccf3413f52236d288ff01d
Al3ncar <alencar29htcss@gmail.com>
2023-03-03 10:06:18 -0300

    Aula 32

--- Aula32/assets/js/index.js
+++ Aula32/assets/js/index.js
@@ -1,22 +1,42 @@
-let cont = document.querySelector('.cont')
+function myEscop(){
+    const cont = document.querySelector('.cont')
 
-const elementos = [
-    {tag: 'p', texto: 'Frase 1'},
-    {tag: 'div', texto: 'Frase 2'},
-    {tag: 'footer', texto: 'Frase 3'},
-    {tag: 'section', texto: 'Frase 4'},
-]
+    const elementos = [
+        {tag: 'p', texto: 'Frase 1'},
+        {tag: 'div', texto: 'Frase 2'},
+        {tag: 'footer', texto: 'Frase 3'},
+        {tag: 'section', texto: 'Frase 4'},
+    ]
+
+    const element = elementos.length // 4
+
+    for(let i = 0; i < element; i++){
+        const {tag, texto} = elementos[i]
+    }
+    
+    function app(tag){
+        const createElements = document.createElement(`${tag}`)  
+        createElements.textContent = `${texto}`  
+        cont.appendChild(`${createElements}`)
+    }app(tag, texto)
+
+}myEscop()
 
 
 
-for(let i = 0; i < elementos.length; i++){
-
-    console.log(elementos[i]);
-}
 
 
-// const [t1, t2, t3, t4 ] = elementos
 
+
+
+
+
+
+
+
+
+
+//const [t1, t2, t3, t4 ] = elementos
 // console.log(elementos.length)
 
 // console.log(t1)
