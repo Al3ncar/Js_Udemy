4de9f85791ef2cccb36aae640ee300b900979eac
Al3ncar <alencar29htcss@gmail.com>
2023-03-03 09:00:47 -0300

    Aula 32

--- Aula32/assets/js/index.js
+++ Aula32/assets/js/index.js
@@ -1,3 +1,5 @@
+let cont = document.querySelector('.cont')
+
 const elementos = [
     {tag: 'p', texto: 'Frase 1'},
     {tag: 'div', texto: 'Frase 2'},
@@ -5,11 +7,19 @@ const elementos = [
     {tag: 'section', texto: 'Frase 4'},
 ]
 
-const [t1, t2, t3, t4 ] = [, [tag, texto], [tag, texto], [tag, texto]] 
 
-console.log(elementos.length)
 
-console.log(t1)
-console.log(t2)
-console.log(t3)
-console.log(t4)
+for(let i = 0; i < elementos.length; i++){
+
+    console.log(elementos[i]);
+}
+
+
+// const [t1, t2, t3, t4 ] = elementos
+
+// console.log(elementos.length)
+
+// console.log(t1)
+// console.log(t2)
+// console.log(t3)
+// console.log(t4)
