e15a5ead4e9f2629127ac6f4074f51db39e558f1
Al3ncar <alencar29htcss@gmail.com>
2023-03-05 20:45:18 -0300

    Aula 33

---
+++ Aula33/assets/js/index.js
@@ -0,0 +1,17 @@
+// Modelo de Objeto de Documento ( DOM )
+
+//O Modelo de Objeto de Documento ( DOM ) é uma interface de programação para documentos HTML, 
+//XML e SVG . Ele fornece uma representação estruturada do documento como uma árvore. 
+//O DOM define métodos que permitem acesso à árvore, para que eles possam alterar a estrutura, 
+//estilo e conteúdo do documento.
+
+// Modelo de Objeto de Documento
+
+/* 
+    //  ARVORE DO DOM
+            WINDOW = PAI DE TODOS
+            DOCUMENT = FILHO DO WINDOW
+            HTML = FILHA DO DOCUMENT
+            BODY = FIPHO DO HTML
+    //
+*/
