c6d69e89f7df5ab79449b4143cbc8f7f11792ac7
Al3ncar <alencar29htcss@gmail.com>
2023-03-07 19:57:05 -0300

    Aula 36

--- Aula36/assets/js/index.js
+++ Aula36/assets/js/index.js
@@ -12,3 +12,28 @@ for(let value of  clothingStore){
 //tendo tambem uma lógica diferente, o of PEGA O VALOR DO ARRAY/CHAVES
 //não sendo necessario colocar:
 // exemplo: console.log( clothingStore[ value ] ), devido o of pegar o valor do indice
+
+
+
+console.log(""); // Quebra de linha no console.
+
+
+
+//Temos tambem um outra maneira de usar o for
+
+clothingStore.forEach( function (value, index, array){
+
+    console.log(value, index, array);
+        // O VALUE é o valor do indice
+        // O INDEX é o proprio indice
+        // O ARRAY é o array completo 
+
+})
+
+
+//OS 4 TIPOS DE FOR:
+
+// 1° FOR I -> manipula até chegar no valor do indice
+// 2° FOR IN -> já possui o indice
+// 3° FOR OF -> já possui o o VALOR do indice
+// 4° FOREACH() -> função que pega o valor e o indice
