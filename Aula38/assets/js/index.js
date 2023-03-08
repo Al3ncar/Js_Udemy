1c422780e1084c1b0896e1758957b2301832a183
Al3ncar <alencar29htcss@gmail.com>
2023-03-08 08:46:34 -0300

    Aula 38

---
+++ Aula38/assets/js/index.js
@@ -0,0 +1,12 @@
+function Escop(){
+
+    const pags = document.querySelectorAll('p')
+        const styleBody = getComputedStyle(document.body)
+        const bodyColor = styleBody.backgroundColor
+
+    for(let index of pags){
+        index.style.backgroundColor = bodyColor
+        index.style.color = '#fff'
+    }
+
+}Escop()
