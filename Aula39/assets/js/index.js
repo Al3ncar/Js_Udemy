a558403cf67aa1fc5a59553f4e438795cf226260
Al3ncar <alencar29htcss@gmail.com>
2023-03-18 22:25:36 -0300

    Aula 39

---
+++ Aula39/assets/js/index.js
@@ -0,0 +1,18 @@
+/* 
+    // While
+
+        A declaração while cria um laço que executa uma rotina especifica enquanto a condição de teste for avaliada como verdadeira. A condição é avaliada antes da execução da rotina.
+
+        sintaxe lógica: 
+
+        while (condição) {
+            rotina
+        }
+
+        while  =>  palavra reservada
+
+        condição  =>  O que vai ser avalido
+            true  =>  Continua o laço de reptição;
+            false =>  Para o laço de repetição
+
+*/
