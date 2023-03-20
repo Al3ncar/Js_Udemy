181aecf497610b9a86b17d250de4ffb91dcc670d
Al3ncar <alencar29htcss@gmail.com>
2023-03-20 10:24:15 -0300

    Aula 40

---
+++ Aula40/assets/js/index.js
@@ -0,0 +1,27 @@
+// BREAK E CONTINU
+
+//Break faz com que meu laço pare
+//Continue faz com que meu laço pule o valor escolhido 
+
+//exemplos:
+
+const SeriaA =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    for(let i of SeriaA){
+        
+
+        // Função Usando Continue 
+        if( i === 2 ){
+            console.log("Estou Pulando o 2");
+            continue;
+        }
+        
+        console.log(i)
+
+        // Função Usando Break
+        if( i === 7 ){
+            console.log("Estou parando no 7");
+            break;
+        }
+
+    }
