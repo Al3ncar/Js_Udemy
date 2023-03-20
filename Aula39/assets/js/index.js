d224f19f8483bb05e1fb14f98752c08a55ebfb09
Al3ncar <alencar29htcss@gmail.com>
2023-03-20 08:23:36 -0300

    Aula 39

--- Aula39/assets/js/index.js
+++ Aula39/assets/js/index.js
@@ -21,18 +21,53 @@
         ;
 
 */
+
 let nome = 'Helena';
 let i = 0;
 let i2 = 0;
 
-while(i <= 10){
-    console.log(i)
-    i++  // Incremento de 0 a 10
-};
+    while(i <= 10){
+        console.log(i)
+        i++  // Incremento de 0 a 10
+    };
 
-console.log("Dando errado")
+console.log("Dando errado");
 
-while (i2 < nome.length) {
-    console.log(nome[i2])
-    i2++
+    while (i2 < nome.length) {
+        console.log(nome[i2])
+        i2++
+    };
+
+/* 
+//Não podemos declarar 2 while com uma mesma declaração 
+
+    let i = 0;
+    let nome = 'Bertinho';
+
+        while(i <= 10){
+            console.log(i)
+            i++  // Incremento de 0 a 10
+        };
+
+        while (i < nome.length) {
+            console.log(nome[i])
+            i++
+        };
+    //caso isso aconteça no console não ira retornar o valor do laço 
+
+*/
+
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
 }
+
+    const min = 1;
+    const max = 10;
+    let rand = random(min, max);
+
+    while( rand !== 5 ){
+        rand = random(min, max);
+        console.log(rand);
+    }
+
