b806750dfcf11da07fef297d54caed4a8cdbbcff
Al3ncar <alencar29htcss@gmail.com>
2023-03-29 06:24:23 -0300

    Aula 45

---
+++ Aula45/assets/js/index.js
@@ -0,0 +1,12 @@
+// try / catch / finally
+
+try{ //  É EXECUTADO QUANDO NÃO HÁ ERROS 
+    console.log(" Abrindo arquivo ")
+    console.log(" Manipulando arquivo ")
+    
+} catch (err){ // É EXECUTADO QUANDO HÁ ERROS 
+    console.log(" Deu erro ao fechar arquivos ")
+    
+} finally{ //  SEMPRE SERÁ EXECUTADO 
+    console.log(" Fechando Arquivo ") 
+}
