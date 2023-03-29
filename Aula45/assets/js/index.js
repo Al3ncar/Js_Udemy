579c240b056ae2b82c933f1781da8cb8da3195d3
Al3ncar <alencar29htcss@gmail.com>
2023-03-29 06:49:57 -0300

    Aula 45

--- Aula45/assets/js/index.js
+++ Aula45/assets/js/index.js
@@ -1,12 +1,42 @@
 // try / catch / finally
 
 try{ //  É EXECUTADO QUANDO NÃO HÁ ERROS 
-    console.log(" Abrindo arquivo ")
-    console.log(" Manipulando arquivo ")
+    console.log("Abrindo arquivo ")
+    console.log("Manipulando arquivo ")
     
 } catch (err){ // É EXECUTADO QUANDO HÁ ERROS 
-    console.log(" Deu erro ao fechar arquivos ")
+    console.log("Deu erro ao fechar arquivos ")
     
 } finally{ //  SEMPRE SERÁ EXECUTADO 
-    console.log(" Fechando Arquivo ") 
+    console.log("Fechando Arquivo ") 
 }
+
+
+    console.log("") // Espaço no console 
+
+
+// Podemos construir estruturas alinhas desta maneira
+// Dentro do try pode vir outro try 
+
+try {
+    console.log("Abrindo arquivo ")
+    console.log("Manipulando arquivo ")
+
+    try {
+        console.log(a)
+    } catch(err) {
+        console.log(" Deu ERRO")
+    } finally {
+        console.log(" Tambem sou FINALLY")
+    }
+
+} catch (err) {
+    console.log("Tratando ERRO ")
+
+}finally{
+    console.log("FINNALY: Sempre serei executado")
+}
+
+
+
+
