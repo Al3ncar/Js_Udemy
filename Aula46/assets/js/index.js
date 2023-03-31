73b2cde91e1fd5f64231a1077068a6b46bb16e94
Al3ncar <alencar29htcss@gmail.com>
2023-03-31 07:41:47 -0300

    Aula 46

--- Aula46/assets/js/index.js
+++ Aula46/assets/js/index.js
@@ -8,16 +8,24 @@
 
 // sintaxe:
 
+// setInterval( o que ira acontecer, tempo que vai acontecer )
+
 
 function mostraHora() {
     let date = new Date();
-
-    return date.toLocaleDateString("pt-BR",{
-        hour12: false
-    });
+    return date.toLocaleTimeString("pt-BR",{ hour12: false });
 }
 
-// setInterval vai configurar um intervalo de tempo, para que alguma função seja executada em determinado Momento 
-setInterval(mostraHora(), 1000)
+function funcaDoIntervalo(){ console.log(mostraHora());}
 
-console.log(mostraHora())
+// setInterval vai configurar um intervalo de tempo, para que alguma função seja executada em determinado Momento 
+//Podemos colocar funções dentro do proprio setInterval 
+
+    setInterval(() => {  console.log( mostraHora() )  }, 1000); 
+
+// Direfença entre chamar a funhção e a referência dela 
+
+    // setInterval(funcaDoIntervalo, 1000); // Passando a referência
+    // setInterval(funcaDoIntervalo(), 1000); // Chamando a função
+
+//
