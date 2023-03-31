e57a7d963ede899e3a5e0c80764d66080f701dae
Al3ncar <alencar29htcss@gmail.com>
2023-03-31 08:36:28 -0300

    Aula 46

--- Aula46/assets/js/index.js
+++ Aula46/assets/js/index.js
@@ -21,7 +21,7 @@ function funcaDoIntervalo(){ console.log(mostraHora());}
 // setInterval vai configurar um intervalo de tempo, para que alguma função seja executada em determinado Momento 
 //Podemos colocar funções dentro do proprio setInterval 
 
-    setInterval(() => {  console.log( mostraHora() )  }, 1000); 
+    let timer = setInterval(() => {  console.log( mostraHora() )  }, 1000); 
 
 // Direfença entre chamar a funhção e a referência dela 
 
@@ -29,3 +29,24 @@ function funcaDoIntervalo(){ console.log(mostraHora());}
     // setInterval(funcaDoIntervalo(), 1000); // Chamando a função
 
 //
+
+
+//setTimeout()
+
+    // O método global setTimeout()define um cronômetro que executa uma função ou trecho de código especificado assim que o cronômetro expira.
+
+//
+
+// sintaxe:
+
+//setTimeout( função a ser executada, executa até um determinado tempo  )
+
+
+    setTimeout( () => { console.log("Olá Munnnddooo!!!") }, 10000)
+    // Deste modo, o "Olá Munnnddooo!!!" só será executado 1 vez, daqui 10 segundos.
+
+//
+
+// existe uma maneira de para o intevalo, sendo ela ClearInterval():
+
+setTimeout(() => { clearInterval(timer) }, 10000)
