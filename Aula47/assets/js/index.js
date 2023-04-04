92cb21ddf6d327ae471582ec37208c44c1ac5f5a
Al3ncar <alencar29htcss@gmail.com>
2023-04-04 11:26:42 -0300

    Aula 47

--- Aula47/assets/js/index.js
+++ Aula47/assets/js/index.js
@@ -10,18 +10,8 @@ function Escop(){
     let minutes = 00
 
     console.log(time)
-    for(let t = 0; t < 60; t++){
-        
-    }
 
-/*     const timeme = setInterval(()=> {
-        second =+ 1;
-        if(second > 60){
-            minutes =+ 1;
-            second = 00;
-        }
 
-    }, 1000) */
     
     iniciar.addEventListener('click', () => {
         relogio.textContent = `${timeme}`
