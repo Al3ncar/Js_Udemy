ed216420e706a4d605746cd6982367449e272e2c
Al3ncar <alencar29htcss@gmail.com>
2023-04-05 00:23:54 -0300

    Aula 47

--- Aula47/assets/js/index.js
+++ Aula47/assets/js/index.js
@@ -5,28 +5,23 @@ function Escop(){
     const pausar = document.querySelector('Pausar');
     const zerar = document.querySelector('Zerar');
 
-    let hours = 00
-    let second = 00
-    let minutes = 00
+    let [ hours, second, minutes ] = [0, 0, 0];
 
-    let time = () => {
-        function zeroAEsquerda(num){return num >= 10 ? num : `0${num}`};
-
-        setInterval(()=> {
-            second < 59 ?  second++ : second = 00;
-            relogio.textContent = `${zeroAEsquerda(hours)} : ${ zeroAEsquerda(minutes) } : ${ zeroAEsquerda(second) }`
-        },1000)
-
-        setInterval(()=>{
-            minutes < 59 ?  minutes++ : minutes = 00
-        },60000)
-
-        setInterval(()=>{
-            hours < 59 ?  hours++ : hours = 00
-        },3600000)
+    const zeroAEsquerda = (num) => {return num >= 10 ? num : `0${num}`};
+    const time = () => {
+        setInterval(()=> { second < 59 ?  second++ : second = 00; }, 1000)
+        setInterval(()=>{ minutes < 59 ?  minutes++ : minutes = 00 }, 60000)
+        setInterval(()=>{ hours < 59 ?  hours++ : hours = 00 }, 3600000)
     }
 
-    
+    let returnTime = setInterval(() => {
+        relogio.textContent = `${zeroAEsquerda(hours)} : ${ zeroAEsquerda(minutes) } : ${ zeroAEsquerda(second) }`
+    }, 1000)
+        
+
+    const stopTime = () => {
+        
+    }
 
     iniciar.addEventListener('click', () => {time()})
 
