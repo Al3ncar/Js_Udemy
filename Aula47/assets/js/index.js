9633502a1a840354022f2a98f0f322d018305589
Al3ncar <alencar29htcss@gmail.com>
2023-04-05 08:20:20 -0300

    Aula 47

--- Aula47/assets/js/index.js
+++ Aula47/assets/js/index.js
@@ -6,25 +6,36 @@ function Escop(){
     const zerar = document.querySelector('.Zerar');
 
     let [ hours, second, minutes ] = [0, 0, 0];
-
+    let buttn = false;
     const zeroAEsquerda = (num) => {return num >= 10 ? num : `0${num}`};
-    const time = () => {
-        setInterval(()=> { second < 59 ?  second++ : second = 00; }, 1000)
-        setInterval(()=>{ minutes < 59 ?  minutes++ : minutes = 00 }, 60000)
-        setInterval(()=>{ hours < 59 ?  hours++ : hours = 00 }, 3600000)
-    }
+    
+    // const time = () => {
+    //     const timeSecond = setInterval(()=> { second < 59 ?  second++ : second = 00; }, 1000);
+    //     const timeMinute = setInterval(()=>{ minutes < 59 ?  minutes++ : minutes = 00 }, 60000);
+    //     const timeHour = setInterval(()=>{ hours < 59 ?  hours++ : hours = 00 }, 3600000);
+    // }
+    
+    // const timeSecond =  () => setInterval(()=> { second < 59 ?  second++ : second = 00; }, 1000);
+    // const timeMinute = () => setInterval(()=>{ minutes < 59 ?  minutes++ : minutes = 00 }, 60000);
+    // const timeHour = () => setInterval(()=>{ hours < 59 ?  hours++ : hours = 00 }, 3600000);
 
-    let returnTime = setInterval(() => {
+
+    if(buttn === true){ setInterval(()=> { second < 59 ?  second++ : second = 00; }, 1000); }
+    if(buttn !== false){ setInterval(()=>{ minutes < 59 ?  minutes++ : minutes = 00 }, 60000) }
+    if(buttn !== false){ setInterval(()=>{ hours < 59 ?  hours++ : hours = 00 }, 3600000) }
+
+    const returnTime = setInterval(() => {
         relogio.textContent = `${zeroAEsquerda(hours)} : ${ zeroAEsquerda(minutes) } : ${ zeroAEsquerda(second) }`
     }, 1000)
         
+    const validado = () => { buttn = true }
 
     const stopTime = () => {
-        console.log(time)
-        setTimeout(() => { clearInterval(time())})
+        setTimeout(() => { clearInterval( timeSecond )})
     }
 
-    iniciar.addEventListener('click', () => {Olá(), Olá2()})
+    // iniciar.addEventListener('click', () => {timeSecond, timeMinute(), timeHour()})
+    iniciar.addEventListener('click', () => {validado()})
     pausar.addEventListener('click', () => {stopTime()})
 
 } Escop()
