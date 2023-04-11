eaa26cfdacd986a907693572285c0001953ef5e1
Al3ncar <alencar29htcss@gmail.com>
2023-04-10 23:18:47 -0300

    Aula 47

--- Aula47/Relogio( D )/assets/js/main.js
+++ Aula47/Relogio( D )/assets/js/main.js
@@ -1,10 +1,8 @@
 function relogio() {
+
     function criaHoraDosSegundos(segundos) {
       const data = new Date(segundos * 1000);
-      return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
-      });
+      return data.toLocaleTimeString('pt-BR', { hour12: false, timeZone: 'UTC' });
     }
   
     const relogio = document.querySelector('.relogio');
