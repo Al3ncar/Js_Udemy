31becea55d722400e382c69d60b18757badf547d
Al3ncar <alencar29htcss@gmail.com>
2023-04-20 15:07:48 -0300

    Aula 40

--- Aula40/assets/js/index.js
+++ Aula40/assets/js/index.js
@@ -1,27 +1,50 @@
-// BREAK E CONTINUE
+// Escreva uma função que receba 2 numeros e retorne o maior deles 
 
-//Break => faz com que meu laço pare
-//Continue => faz com que meu laço pule o valor escolhido 
+// Usando numeros aleatorios
+function Escop(){
 
-//exemplos:
+    function random(n1, n2, n3, n4){
+        const r = (Math.random() * (n1 -  n2) + n2) 
+        const r2 = Math.random() * (n3 -  n4) + n4
+        const RS = [Math.floor(r), Math.floor(r2)]
+        return RS
+    }
 
-const SeriaA =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const [n1, n2] = [1, 10];
+    const [n3, n4] = [1, 10];
+    let rand = random(n1, n2, n3, n4);
 
-    for(let i of SeriaA){
-
-        
-        // Função Usando Continue 
-        if( i === 2 ){
-            console.log("Estou Pulando o 2");
-            continue;
-        }
-        
-        console.log(i);
-
-        // Função Usando Break
-        if( i === 7 ){
-            console.log("Estou parando no 7");
+    for(let i of rand){
+        console.log(i)
+        if(rand[0] > rand[1]){
+            console.log(rand[0], `O valor ${rand[0]} é maior que ${rand[1]}`);
+            break;
+        }else if(rand[0] < rand[1]){
+            console.log(rand[1], `O valor ${rand[1]} é maior que ${rand[0]}`);
+            break;
+        }else if(rand[0] === rand[1]){
+            console.log('Ambos valores são iguais')
             break;
         }
-
     }
+    
+}Escop()
+
+// Escolhento os numeros
+
+
+function Escop2(){
+
+    let n1 = 100; let n2 = 20;
+
+    function V (n1, n2){
+        n1 > n2 ?console.log( `${n1} é maior que ${n2}`) : console.log(`${n2} é maior que ${n1}`) 
+    }V(n1, n2)
+
+}Escop2()
+
+// CODIGO DA AULA 
+
+
+const max = (x, y) => x > y ? x : y;
+console.log(max(10, 3)) 
