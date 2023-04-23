18b02d198bdb7293919466c489eee1bbc53c20de
Al3ncar <alencar29htcss@gmail.com>
2023-04-22 22:52:37 -0300

    Aula 42

--- Aula42/assets/js/index.js
+++ Aula42/assets/js/index.js
@@ -1,13 +1,50 @@
-// Escreva uuma função chamada ePaisagem que recebe dois argumentos, largura e altura de uma imagem ( Number ).
-// Retorne true se a imagem estiver no modo paisagem. 
+// Escreva uma função que recebe um numero e retorna o seguinte:
+//Número é divisivel por 3 = Fizz
+//Número é divisivel por 5 = Buzz
+//Número é divisivel por 3 e 5 = FizzBuzz
+//Número NÃO é divisivel por 3 e 5 = Retorna o próprio número
 
-    function ePaisagemFun ( lag, alt ) { return lag > alt ? true : false; }
-    console.log(ePaisagemFun(150, 50)) 
+// Checar se o numero é realmente um numero =  Retorna o PROPRIO VALOR RECEBIDO 
+// Use a função com o numero de 0 a 100 
 
-    const ePaisagemArr = (lag, alt) => lag > alt ? console.log(`Modo Paisagem`, true): console.log(`Modo vertical`, false);
-    ePaisagemArr(150, 50) 
+function rNumb (x) {
+    if(x % 3 === 0 && x % 5 === 0){ console.log(`FIZZBUZZ`)}else
+    if(x % 3 === 0){ console.log(`FIZZ`)}else
+    if(x % 5 === 0){ console.log(`BUZZ`)}else
+    if(x !== true ){ console.log(`Seu numero é: ${x}`)};
+}
+for(let i = 0; i <= 10; i++){ console.log(i, rNumb(i)) }
 
-    // # # Codigo de aula # # //
 
-    const ePaisagemArr2 = (largura, altura) => largura > altura;
-    console.log(ePaisagemArr2(1980, 1080)) // A função já irá retornar true ou falso por padrão
+console.log("") // Espaço no console
+
+
+// # # Codigo da Aula # # //
+
+function fizzBuzz(numero){
+    if(typeof numero !== 'number') return numero;
+    if(numero % 3 === 0 && numero % 5 === 0) return `FIZZBUZZ`;
+    if(numero % 3 === 0) return `FIZZ`;
+    if(numero % 5 === 0) return `BUZZ`;
+    return numero;
+}
+
+
+console.log('a', fizzBuzz('a'))
+for(let i = 0; i <= 10 ; i++){ 
+    console.log(i, fizzBuzz(i))
+}
+
+console.log("") // Espaço no console
+
+
+
+// # # Melhoria no meu codigo # # //
+
+function rNumb2 (x) {
+    if(x % 3 === 0 && x % 5 === 0)return `FIZZBUZZ`;
+    if(x % 3 === 0)return`FIZZ`;
+    if(x % 5 === 0)return `BUZZ`;
+    if(x !== true )return `Seu numero é: ${x}`;
+}
+for(let i = 0; i <= 10; i++){ console.log(i, rNumb2(i)) }
