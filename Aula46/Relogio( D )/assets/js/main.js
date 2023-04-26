c15688d9cdcc601df9bbb4110a08334fea955894
Al3ncar <alencar29htcss@gmail.com>
2023-04-26 09:44:27 -0300

    Aula 46

--- Aula46/Relogio( D )/assets/js/main.js
+++ Aula46/Relogio( D )/assets/js/main.js
@@ -1,4 +1,4 @@
-function relogio() {
+/* function relogio() {
 
     function criaHoraDosSegundos(segundos) {   // função responsavel por retornar o Tempo 
       const data = new Date(segundos * 1000);  // Pegando a hora do inicio da era onix 
@@ -10,14 +10,10 @@ function relogio() {
     let timer;  // Guardando o setInterval()
   
     function iniciaRelogio() {  // Função responsavel por iniciar o relogio
-
       timer = setInterval(function() {  // SetInterval responsavel por rolar o tempo
-
         segundos++;   // somando a let segundos 
         relogio.innerHTML = criaHoraDosSegundos(segundos);  // mostrando a pagina o tempo executado
-
       }, 1000);
-
     }
   
     document.addEventListener('click', function(e) { // Evento adicionado de click 
@@ -42,5 +38,41 @@ function relogio() {
         classes.add('pausado');
       }
     });
+  } relogio();
+ */
+
+// ----------------  Maneira Simples ---------------------- //
+
+
+  function criaHoraDosSegundos(segundos) {  
+    const data = new Date(segundos * 1000);  
+    return data.toLocaleTimeString('pt-BR', { hour12: false, timeZone: 'UTC' }); 
   }
-  relogio();
+
+  const relogio = document.querySelector('.relogio');
+  const iniciar = document.querySelector('.iniciar');
+  const pausar = document.querySelector('.pausar');
+  const zerar = document.querySelector('.zerar');
+  let segundos = 0;
+  let timer;
+
+  function iniciaRelogio() { 
+    timer = setInterval(function() {  
+      segundos++;   
+      relogio.innerHTML = criaHoraDosSegundos(segundos);  
+    }, 1000);
+  }
+
+  iniciar.addEventListener('click', function(event) {
+    clearInterval(timer);
+    iniciaRelogio();
+  });
+
+  pausar.addEventListener('click', function(event) {
+    clearInterval(timer);
+  });
+
+  zerar.addEventListener('click', function(event) {
+    clearInterval(timer);
+    segundos = 0; relogio.innerHTML = '00:00:00'
+  });
