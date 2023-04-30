e1dbc90c2b51b3082ec18de7aae5b1a69f9844df
Al3ncar <alencar29htcss@gmail.com>
2023-04-29 23:16:13 -0300

    Aula 48

---
+++ Aula48/assets/js/index.js
@@ -0,0 +1,26 @@
+function tesk(){
+    let cont = document.querySelector('.container');
+    let inp = document.querySelector('.input-nova-tarefa');
+    let btn = document.querySelector('.btn-add-tarefa');
+    let taref = document.querySelector('.tarefas');
+
+    let Val;
+
+    function ValResult (Val){
+        if(Val === false) return 'sei';
+        if(Val === true ) return 'Ahta ';
+        return Val
+    }
+
+    document.addEventListener('click', (e) => {
+        const el = e.target;
+        Val = inp.value;
+
+        if(el.classList.contains('btn-add-tarefa')){
+
+            
+            taref.innerHTML += `<li>${Val}</li>` + `<button>Apagar</button>`
+        }
+    })
+
+} tesk()
